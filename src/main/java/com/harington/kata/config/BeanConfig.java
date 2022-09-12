c831e89d56616b2a04e2701ce8ae107225f734bd
Hassan <hassan.saghir.2022@gmail.com>
2022-09-12 23:52:53 +0100

    feat: mapper configuration

---
+++ src/main/java/com/harington/kata/config/BeanConfig.java
@@ -0,0 +1,16 @@
+package com.harington.kata.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class BeanConfig {
+
+    @Bean
+    @Primary
+    public ModelMapper mapper() {
+        return new ModelMapper();
+    }
+}
