292dbf290640b267a933af3462f9a4a80ec3f112
Hassan <hassan.saghir.2022@gmail.com>
2022-09-12 15:00:48 +0100

    feat: create account entity

---
+++ src/main/java/com/harington/kata/entity/Account.java
@@ -0,0 +1,26 @@
+package com.harington.kata.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name="accounts")
+
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String rib;
+    private String swift;
+    private String iban;
+    private double amount;
+
+    @ManyToOne()
+    @JoinColumn(name = "account_id")
+    private Customer customer;
+}
---
+++ src/main/java/com/harington/kata/entity/Customer.java
@@ -0,0 +1,35 @@
+package com.harington.kata.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Table(name="customers")
+
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(unique = true)
+    private String email;
+
+    private LocalDate birthday;
+
+    @OneToMany(mappedBy = "customer", cascade = CascadeType.REMOVE)
+    private Set<Account> accounts;
+
+}
