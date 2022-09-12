2796115cccc4a883b9fc9706c3b4e293a26a1d0e
Hassan <hassan.saghir.2022@gmail.com>
2022-09-12 15:12:07 +0100

    feat: create Customer and Account Repository

---
+++ src/main/java/com/harington/kata/repository/AccountRepository.java
@@ -0,0 +1,7 @@
+package com.harington.kata.repository;
+
+import com.harington.kata.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
+}
