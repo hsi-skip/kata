192fff0276477496687bb6a8d907d38f4fc3988f
Hassan <hassan.saghir.2022@gmail.com>
2022-09-12 23:52:25 +0100

    feat: Redefine save methode

--- src/main/java/com/harington/kata/repository/AccountRepository.java
+++ src/main/java/com/harington/kata/repository/AccountRepository.java
@@ -4,4 +4,7 @@ import com.harington.kata.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
+
+    Account save(Account account);
+
 }
