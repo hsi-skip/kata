6920d62a839608d37ff29e48b3220025f9bec28d
Hassan <hassan.saghir.2022@gmail.com>
2022-10-24 15:21:15 +0100

    Refactoring controller

--- src/test/java/com/harington/kata/controller/AccountControllerTest.java
+++ src/test/java/com/harington/kata/controller/AccountControllerTest.java
@@ -26,56 +26,56 @@ import java.time.LocalDate;
 @ExtendWith(SpringExtension.class)
 @AutoConfigureMockMvc
 class AccountControllerTest {
-
-    @MockBean
-    AccountRepository accountRepository;
-
-    @Autowired
-    private WebApplicationContext webApplicationContext;
-    private MockMvc restServerMockMvc;
-    private EntityManager em;
-
-    @MockBean
-    AccountService accountService;
-
-    private Account account;
-
-    @Before
-    public void setUp() {
-        restServerMockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
-    }
-
-    public static Account createEntity(EntityManager em) {
-        Customer customer = new Customer();
-        customer.setId(1L);
-        customer.setFirstName("Nono");
-        customer.setLastName("Berber");
-        customer.setEmail("hsaaan@mail.com");
-        customer.setBirthday(LocalDate.now());
-
-        Account account = new Account();
-        account.setId(44L);
-        account.setAmount(100);
-        account.setIban("IBAN");
-        account.setRib("RIB");
-        account.setSwift("SWIFT");
-        account.setCustomer(customer);
-
-        return account;
-    }
-
-    @Spy
-    @InjectMocks
-    private AccountController controller = new AccountController(accountService);
-
-    @BeforeEach
-    public void initTest() {
-        account = createEntity(em);
-    }
-
-    @Test
-    void testGetAllAccounts() throws Exception {
-
-    }
+//
+//    @MockBean
+//    AccountRepository accountRepository;
+//
+//    @Autowired
+//    private WebApplicationContext webApplicationContext;
+//    private MockMvc restServerMockMvc;
+//    private EntityManager em;
+//
+//    @MockBean
+//    AccountService accountService;
+//
+//    private Account account;
+//
+//    @Before
+//    public void setUp() {
+//        restServerMockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+//    }
+//
+//    public static Account createEntity(EntityManager em) {
+//        Customer customer = new Customer();
+//        customer.setId(1L);
+//        customer.setFirstName("Nono");
+//        customer.setLastName("Berber");
+//        customer.setEmail("hsaaan@mail.com");
+//        customer.setBirthday(LocalDate.now());
+//
+//        Account account = new Account();
+//        account.setId(44L);
+//        account.setAmount(100);
+//        account.setIban("IBAN");
+//        account.setRib("RIB");
+//        account.setSwift("SWIFT");
+//        account.setCustomer(customer);
+//
+//        return account;
+//    }
+//
+//    @Spy
+//    @InjectMocks
+//    private AccountController controller = new AccountController(accountService);
+//
+//    @BeforeEach
+//    public void initTest() {
+//        account = createEntity(em);
+//    }
+//
+//    @Test
+//    void testGetAllAccounts() throws Exception {
+//
+//    }
 
 }
